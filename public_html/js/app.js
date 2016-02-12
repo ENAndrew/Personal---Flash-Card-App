b0e44f04af78c6360cb8f38fa60d801a2789193f
Erica Andrew <erica.nicole.andrew@gmail.com>
2016-02-12 09:10:36 -0700

    Add working files to repo

---
+++ public_html/js/app.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+   
+   
+    var signsMaster = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Want', 'Water', 'Airplane'];
+    
+    var signsNeedWork = [];
+    
+    var signsKnown = [];
+    
+    //I don't think i need hidden divs, I can just switch arrays each time, and insert the appropriate text.
+    
+    //main function - will need to take a random item from the array, and put the item into the html text placeholder.  
+    
+    //header buttons switches which array being used
+    
+    //next button chooses a new random item from chosen array
+    
+    //easy pushes to signs known
+    
+    // need work pushes to signs not known
+    
+    
+});
